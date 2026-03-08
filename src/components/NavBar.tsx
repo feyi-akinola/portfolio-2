d728d818e24252f0086912e36464da96ee18fa1e
Feyi Akinola <feyi.akinola.3005@gmail.com>
2026-03-08 13:43:22 +0100

    Download resume in nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,8 @@
-const NAV_LINKS = ["ABOUT", "PROJECTS", "CONTACT"] as const;
+type Link = { name: string; url: string };
+
+const NAV_LINKS: Link[] = [
+  { name: "Download Resume", url: "/resume/Feyisayo_Akinola_Resume.pdf" }
+] as const;
 
 export default function NavBar() {
   return (
@@ -8,13 +12,16 @@ export default function NavBar() {
         <span className="font-bold">AKINOLA</span>
       </div>
       <div className="flex gap-14">
-        {NAV_LINKS.map((link) => (
+        {NAV_LINKS.map(({ name, url }) => (
           <a
-            key={link}
-            href="#"
-            className="text-white text-xs font-medium tracking-widest opacity-75 button-text-light_ no-underline"
+            key={name}
+            href={url}
+            target="_blank"
+            download
+            className="text-white text-xs font-medium tracking-widest
+              opacity-75 button-text-light_ no-underline"
           >
-            {link}
+            {name}
           </a>
         ))}
       </div>
